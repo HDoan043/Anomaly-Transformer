a928af31e9a5e82ec1f3b531967bdc5916a0863e
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-04-23 20:21:27 +0700

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -15,3 +15,33 @@ def to_var(x, volatile=False):
 def mkdir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+class ProgressBar():
+    def __init__(self, iteration, bin = 100):
+        self._iteration = iteration
+        self.postfix = ""
+        self.bin = bin
+
+    def __iter__(self):
+        self.len_pre_print = 0
+        for i, item in enumerate(self._iteration):
+            self.i = i
+            self.show()
+            yield item
+        print()
+    
+    def show(self):
+        percentage = self.i/len(self._iteration)
+        progress = "\r["+"="*round(percentage*self.bin) + "-"*(self.bin-round(percentage*self.bin))+f"] {round(percentage*100)}% "+ self.postfix 
+        len_current_print = len(progress)
+        end_blank = (self.len_pre_print-len_current_print) if len_current_print<self.len_pre_print else 0
+        print(progress + " "*end_blank, end="")
+        self.len_pre_print =  len_current_print
+        
+    def set_postfix(self, postfix={}):
+        postfix_ls = []
+        for key, value in postfix.items():
+            postfix_ls.append(f" {key} : {value}")
+        if len(postfix_ls)>0:
+            self.postfix = "[" + ",".join(postfix_ls) + "]"
+            self.show()
