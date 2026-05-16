9699320b4894c3a2f46635cb3cd88b5f95ba7827
HDoan043 <165142987+HDoan043@users.noreply.github.com>
2026-05-16 17:02:38 +0700

    Add adjustment function for anomaly detection

--- utils/utils.py
+++ utils/utils.py
@@ -45,3 +45,26 @@ class ProgressBar():
         if len(postfix_ls)>0:
             self.postfix = "[" + ",".join(postfix_ls) + "]"
             self.show()
+            
+def adjustment(gt, pred):
+    anomaly_state = False
+    for i in range(len(gt)):
+        if gt[i] == 1 and pred[i] == 1 and not anomaly_state:
+            anomaly_state = True
+            for j in range(i, 0, -1):
+                if gt[j] == 0:
+                    break
+                else:
+                    if pred[j] == 0:
+                        pred[j] = 1
+            for j in range(i, len(gt)):
+                if gt[j] == 0:
+                    break
+                else:
+                    if pred[j] == 0:
+                        pred[j] = 1
+        elif gt[i] == 0:
+            anomaly_state = False
+        if anomaly_state:
+            pred[i] = 1
+    return gt, pred
